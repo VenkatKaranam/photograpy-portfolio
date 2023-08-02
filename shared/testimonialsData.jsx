07f56ef7651c6395600c58d3b7535722489bd8ae
Venkat Karanam <karanamvasu871@gmail.com>
2023-08-02 21:56:34 +0530

    changes

--- shared/testimonialsData.jsx
+++ shared/testimonialsData.jsx
@@ -1,31 +1,31 @@
 export const testimonialsData = [
     {
-        name : 'Vinay Kumar',
+        name : 'Chiranjeevi',
         designation : 'Software Engineer',
         review : '100% satisfied! Amazing candid pics, committed to delivering more than expected. Experienced crew with top equipment. Highly recommended for capturing good moments!',
     },
     {
-        name : 'Vinay Kumar',
+        name : 'Saketh',
         designation : 'Software Engineer',
         review : 'Beautiful photos, beautiful candid pics, beautiful prewedding shoot and every thing is beautiful.. Thanx for making our memories more more moreee beautiful.',
     },
     {
-        name : 'Vinay Kumar',
+        name : 'Alekhya',
         designation : 'Software Engineer',
         review : 'Amazing experience, the photos and videos were beautifully captured, really loved the work. thanks a lot for wonderful memories of our life',
     },
     {
-        name : 'Vinay Kumar',
+        name : 'Hitesh',
         designation : 'Software Engineer',
         review : 'After contacting more than a dozen wedding photographers, we finally booked VJ rishi photography because we could see their vision and how passionate they are about their work. And after a year, we can say that we were right about them',
     },
     {
-        name : 'Vinay Kumar',
+        name : 'Santhosh',
         designation : 'Software Engineer',
         review : 'Best of all Go for it if you want to cherish Ur memorable moments beautifully Friendly staff✌🏻 Skilled and hard working. It was worth it and amazing. It was the great decision i made. Teamwork was great',
     },
     {
-        name : 'Vinay Kumar',
+        name : 'Ravikanti',
         designation : 'Software Engineer',
         review : 'The photographer was an absolute professional. They were able to capture every special moment of our wedding day in a way that will allow us to relive the day over and over again.',
     }
